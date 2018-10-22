2483ac991735519dc77d28820982919e35c76df3
joshuacork293@yahoo.com <>
2018-10-22 15:26:29 -0400

    First commit

---
+++ src/githubtest/GithubTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubtest;
+
+/**
+ *
+ * @author jrc62
+ */
+public class GithubTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Cork();
+    }
+    public static void Cork()
+    {
+        System.out.println("Joshua Cork");
+    }
+    
+}
